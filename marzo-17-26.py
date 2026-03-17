b648ac5a1d74794304d98f674f44fca5526a0e10
juanpablorubiod-lab <juanpablo.rubiod.jss@gmail.com>
2026-03-17 08:48:57 -0600

    marzo-17

---
+++ marzo-17-26.py
@@ -0,0 +1,26 @@
+# a= float (input ("Pon un número:"))
+#b= float (input ("Pon otro número:")) 
+a= 5
+b= 10
+
+def resta (a, b):
+    return a - b 
+def suma (a, b):
+    return a + b
+def multiplicación (a, b): 
+    return a*b
+  
+def división (a, b):
+    return a/b
+print (f"Multiplicación = {multiplicación (a, b)}")
+print (f"Suma = {suma (a, b)}")
+print (f"Resta = {resta (a, b)}")
+
+ 
+def fun1 ():
+    x= 7
+    print (f"Valor de x = {x}")
+
+x=15
+fun1()
+print (f"Valor de x= {x}")
